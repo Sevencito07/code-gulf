8e09a83513b81f9bb4861b613b3fb53c4dc0d3a1
cortes <cortesseven2003@gmail.com>
2022-10-27 17:09:19 -0500

    Updates

--- supabase/Client.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-
- export const supabase =    createClient(
-   'https://ozububbwrvqepuhyrwbx.supabase.co',
-   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dWJ1YmJ3cnZxZXB1aHlyd2J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUyODA1NzEsImV4cCI6MTk4MDg1NjU3MX0.nWUPqRkLK995lM_ie9B_lo-SffhHEe4mRy8YtEYoIf0'
-    )
